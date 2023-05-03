54bd8405be0987380d2a05a51678a859d9ecb20f
Sang Uk Jung <83222288+JSU0318@users.noreply.github.com>
2023-05-03 15:55:30 +0900

    TSprac2

--- index.ts
+++ index.ts
@@ -9,9 +9,23 @@ Ham(123);
 type Member = [number, boolean];
 let john: Member = [123, true];
 
+let 이름: string = "kim";
+let 나이: number = 20;
+let 결혼했니: boolean = false;
+
 class User {
   name: string;
   constructor(name: string) {
     this.name = name;
   }
 }
+
+let project: {
+  member: string[];
+  days: number;
+  started: boolean;
+} = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
